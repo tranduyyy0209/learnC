551c07ab239850cbcac69dddca9ad7a4b7afb0bb
Duy <tranduy10a@gmail.com>
2024-07-12 18:59:39 +0700

    first commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello, World!\n");
+
+    //     git init
+    // git add .
+    // git commit -m "first commit"
+    // git branch -M main
+    // git remote add origin https://github.com/tranduyyy0209/learnC.git
+    // git push -u origin main
+    return 0;
+}
